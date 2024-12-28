c9688909e5659bf4ba71bee6cdbfe0ae9779ecd7
Tawseef Rahman <tawseefrahman1@gmail.com>
2024-12-27 21:51:52 -0500

    Updated the vite.config.js file to keep the CNAME file for every npm run build command

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/tr-website/",
+
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      plugins: [
+        {
+          name: "add-cname",
+          writeBundle() {
+            // Copy the CNAME file into the dist/ folder after build
+            fs.copyFileSync(
+              resolve(__dirname, "CNAME"),
+              resolve(__dirname, "dist", "CNAME")
+            );
+          },
+        },
+      ],
+    },
+  },
 });
